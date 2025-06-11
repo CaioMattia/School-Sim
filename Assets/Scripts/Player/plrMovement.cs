55e7c74bcd8206ce3a2013c289b76245d81fa84b
Caio <145506559+CaioMattia@users.noreply.github.com>
2025-06-11 18:40:41 -0300

    a

--- Assets/Scripts/Player/plrMovement.cs
+++ Assets/Scripts/Player/plrMovement.cs
@@ -9,6 +9,7 @@ public class plrMovement : MonoBehaviour
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false;
         animator = GetComponent<Animator>();  // Pega o Animator no mesmo GameObject
         Debug.Log("Animator inicializado.");
     }
@@ -47,19 +48,19 @@ public class plrMovement : MonoBehaviour
         if ((inputDirection != Vector3.zero) && (!Input.GetKey((KeyCode.LeftShift))))
         {
             moveSpeed = 4f;
-            animator.CrossFade("walk", 0.014f);
+            animator.CrossFade("walk", 0.01f);
             Debug.Log("Estado: Andando");
         }
         else if ((inputDirection != Vector3.zero) && (Input.GetKey((KeyCode.LeftShift))))
         {
             moveSpeed = 6f;
-            animator.CrossFade("run", 0.014f);
+            animator.CrossFade("run", 0.01f);
             Debug.Log("Estado: Correndo");
         }
         else
         {
             moveSpeed = 4f;
-            animator.CrossFade("idle", 0.014f);
+            animator.CrossFade("idle", 0.01f);
             Debug.Log("Estado: Parado");
         }
     }
